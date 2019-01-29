0fde1afbb4394806415e51505c42b5394df1241f
deleted-user <>
2019-01-29 11:29:30 -0800

    Added Settings and updated .gitignore

---
+++ OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/Settings.cs
@@ -0,0 +1,27 @@
+﻿/*
+Author: ***REMOVED*** ***REMOVED***
+Contributors: N/A
+Date Last Edited: 1/29/2019
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+	public class KeyBinds
+	{
+		public KeyCode up = KeyCode.W;
+		public KeyCode down = KeyCode.S;
+		public KeyCode left = KeyCode.A;
+		public KeyCode right = KeyCode.D;
+		
+		public KeyCode dash = KeyCode.LeftShift;
+
+		public KeyCode attack = KeyCode.Space;
+
+		public KeyCode previousItem = KeyCode.Q;
+		public KeyCode nextItem = KeyCode.E;
+	}
+}
