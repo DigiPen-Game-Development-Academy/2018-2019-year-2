369e46ea802241b95d2f2745dd4dec4d4bf9c9ce
deleted-user <>
2019-01-29 10:52:07 -0800

    Added FormatExample.cs

---
+++ OfficeDungeonCrawler/FormatExample.cs
@@ -0,0 +1,28 @@
+/*
+Author: ***REMOVED***
+Contributors: N/A
+Last Edit: 1/29/2019
+*/
+
+// Class used for memez
+class E
+{
+	// A very kewl variable
+	float coolVariable = 9001.0f;
+	
+	// Tests is variable is over 9000
+	void Derp()
+	{
+		// If over 9000
+		if (coolVariable > 9000.0f)
+		{
+			// E
+			Debug.Log("ITZ OVR 90000000!!!!1!!!1!!!");
+			return;
+		}
+		
+		// Otherwise big oof
+		if (coolVariable <= 9000.0f)
+			Debug.Log("variabl is lame. big oof.");
+	}
+}
