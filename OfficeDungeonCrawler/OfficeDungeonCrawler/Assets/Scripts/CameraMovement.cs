5058e242693f6fae6fa565189c793f3f43f1f90e
deleted-user <>
2019-02-08 11:02:51 -0800

    Added CameraMovement script

---
+++ OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CameraType
+{
+	Locked,
+	Lerped,
+	Directional
+}
+
+public class CameraMovement : MonoBehaviour
+{
+	public GameObject target;
+	public float speed = 3.0f;
+	public float directionMultiplier = 2.5f;
+	public CameraType type = CameraType.Lerped;
+
+	PlayerMovement playerMovement;
+
+	void Start()
+	{
+		playerMovement = target.GetComponent<PlayerMovement>();
+	}
+	
+	void FixedUpdate()
+	{
+		Vector3 position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+		if (type == CameraType.Directional)
+			position += playerMovement.direction * directionMultiplier;
+
+		if (type == CameraType.Locked)
+			transform.position = position;
+		else
+			transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime * speed);
+	}
+}
