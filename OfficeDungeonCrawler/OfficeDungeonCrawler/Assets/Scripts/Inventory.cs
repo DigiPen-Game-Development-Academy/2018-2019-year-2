91d8c45131e9f3788fc4c3f2a1120e9159339a71
deleted-user <>
2019-03-08 14:26:41 -0800

    Updated Inventory

--- OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/Inventory.cs
+++ OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/Inventory.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
@@ -8,20 +9,63 @@ public class Inventory : MonoBehaviour
 	public List<Item> items = new List<Item>();
 	public List<Item> itemBases = new List<Item>();
 
-	public GameObject slot1;
-	public GameObject slot2;
-	public GameObject slot3;
-	public GameObject slot4;
-	public GameObject slot5;
+	public int selectedSlot = 0;
+
+	public List<GameObject> slots = new List<GameObject>();
+	public List<GameObject> slotBackgrounds = new List<GameObject>();
+
+	public Sprite slotBackgroundSelected;
+	public Sprite slotBackgroundDeselected;
+	public Sprite slotBackgroundDeselectedL;
+	public Sprite slotBackgroundDeselectedR;
 
 	void Start()
 	{
-		
+		GiveItem("test1", 1);
+		GiveItem("test2", 1);
+		GiveItem("test3", 1);
+		GiveItem("test4", 1);
+		GiveItem("test5", 1);
 	}
 	
 	void Update()
 	{
-		
+		Debug.Log("Slot: " + selectedSlot);
+
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			if (selectedSlot > 0)
+				--selectedSlot;
+			else
+				selectedSlot = slots.Count - 1;
+		}
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			if (selectedSlot < slots.Count - 1)
+				++selectedSlot;
+			else
+				selectedSlot = 0;
+		}
+
+		for (int i = 0; i < slotBackgrounds.Count; ++i)
+		{
+			if (i == selectedSlot)
+				slotBackgrounds[i].GetComponent<Image>().sprite = slotBackgroundSelected;
+			else
+			{
+				if (i == 0)
+					slotBackgrounds[i].GetComponent<Image>().sprite = slotBackgroundDeselectedL;
+				else if (i == slotBackgrounds.Count - 1)
+					slotBackgrounds[i].GetComponent<Image>().sprite = slotBackgroundDeselectedR;
+				else
+					slotBackgrounds[i].GetComponent<Image>().sprite = slotBackgroundDeselected;
+			}
+		}
+
+		for (int i = 0; i < slots.Count; ++i)
+		{
+			slots[i].GetComponent<Image>().sprite = items[i].sprite;
+		}
 	}
 
 	public void GiveItem(string id, int count)
