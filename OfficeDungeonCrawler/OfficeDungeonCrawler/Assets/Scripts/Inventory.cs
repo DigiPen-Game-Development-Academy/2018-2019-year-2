0b48ff2288a6dec48fe6fd97fdf6691a3b735a57
deleted-user <>
2019-03-08 12:57:22 -0800

    Added level and inventory

---
+++ OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/Inventory.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+	[HideInInspector]
+	public List<Item> items = new List<Item>();
+	public List<Item> itemBases = new List<Item>();
+
+	public GameObject slot1;
+	public GameObject slot2;
+	public GameObject slot3;
+	public GameObject slot4;
+	public GameObject slot5;
+
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		
+	}
+
+	public void GiveItem(string id, int count)
+	{
+		foreach (Item item in items)
+		{
+			if (item.id == id)
+			{
+				item.count += count;
+				return;
+			}
+		}
+
+		if (items.Count < 5)
+		{
+			Item newItem = null;
+			foreach (Item item in itemBases)
+			{
+				if (item.id == id)
+					newItem = item.Clone();
+			}
+
+			if (newItem == null)
+			{
+				Debug.Log("No item with id " + id + " exists!");
+				return;
+			}
+
+			newItem.count = count;
+
+			items.Add(newItem);
+		}
+		else
+			Debug.Log("Inventory full!");
+	}
+
+	public void RemoveItem(string id, int count)
+	{
+		foreach (Item item in items)
+		{
+			if (item.id == id)
+			{
+				item.count -= count;
+
+				if (item.count <= 0)
+					items.Remove(item);
+
+				return;
+			}
+		}
+	}
+
+	public void Print()
+	{
+		string str = "Inv: [";
+		for (int i = 0; i < items.Count; ++i)
+		{
+			str += "{" + items[i].id + ", " + items[i].count + "}";
+			if (i < items.Count - 1)
+				str += ", ";
+		}
+		str += "]";
+		Debug.Log(str);
+	}
+}
---
+++ OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/Item.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+	NoUse = 0,
+	MeleeWeapon = 1,
+	RangedWeapon = 2,
+	HealthItem = 3
+}
+
+[Serializable]
+public class Item
+{
+	public string id;
+	public string name;
+	public ItemType itemType = ItemType.NoUse;
+
+	public Sprite sprite;
+
+	public int maxCount = 1;
+	public int count = 1;
+	public float damage = 1.0f;
+	public float health = 1.0f;
+
+	public Item(string id_, string name_, int maxCount_, ItemType itemType_, float amount_, Sprite sprite_)
+	{
+		id = id_;
+		name = name_;
+		maxCount = maxCount_;
+		itemType = itemType_;
+		sprite = sprite_;
+
+		if (itemType_ == ItemType.MeleeWeapon || itemType_ == ItemType.RangedWeapon)
+			damage = amount_;
+		else if (itemType_ == ItemType.HealthItem)
+			health = amount_;
+	}
+
+	public Item(string id_, string name_, int maxCount_, ItemType itemType_, float damage_, float health_, Sprite sprite_)
+	{
+		id = id_;
+		name = name_;
+		maxCount = maxCount_;
+		itemType = itemType_;
+		sprite = sprite_;
+		
+		damage = damage_;
+		health = health_;
+	}
+
+	public Item Clone()
+	{
+		return new Item(id, name, maxCount, itemType, damage, health, sprite);
+	}
+}
