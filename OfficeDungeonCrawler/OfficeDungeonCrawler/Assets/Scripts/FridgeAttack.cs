61872c13a9208641198b6b51be725c7fab877db6
deleted-user <>
2019-03-26 13:43:38 -0700

    Justin don't forget to put the f after a float.

--- OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/FridgeAttack.cs
+++ OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/FridgeAttack.cs
@@ -14,7 +14,7 @@ public class FridgeAttack : MonoBehaviour
     public GameObject splashHitbox;
 
     public float attackDistance = 1;
-    public float refridgerator = 6.0;
+    public float refridgerator = 6.0f;
     public float attackCooldown = 1.0f;
     public float attackChargeTime = 0.5f;
     public float attackDamage = 1.0f;
