bbeec6ced6e8d460a54c2067ff130b2cdcdd1af7
deleted-user <>
2019-04-02 14:36:27 -0700

    e

--- OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/PlayerAttack.cs
+++ OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/PlayerAttack.cs
@@ -9,18 +9,18 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Stores the hitbox object we spawn
-    public GameObject hitbox;
+	// Stores the hitbox object we spawn
+	public GameObject hitbox;
 
-    // Public variable we can edit in inspector
-    public float attackSpeed;
-    // Private variable we can edit without effecting the public variable
-    private float attackCooldown;
-    // Public variable for how much damage the player does
-    public float attackDamage = 1.0f;
+	// Public variable we can edit in inspector
+	public float attackSpeed;
+	// Private variable we can edit without effecting the public variable
+	private float attackCooldown;
+	// Public variable for how much damage the player does
+	public float attackDamage = 1.0f;
 	public float attackAnimTime = 0.5f;
 	float timeTillResetAnim = 0.0f;
-  
+
 	PlayerMovement playerMovement;
 
 	Animator animator;
@@ -31,80 +31,68 @@ public class PlayerAttack : MonoBehaviour
 
 	Camera cameraComponent;
 
-    //Vector2 spawnPos = new Vector2();
-    // this catAttack checks if the player is allowed to attack currently only being used from the movement script
-    public bool canAttack = true;
+	//Vector2 spawnPos = new Vector2();
+	// this catAttack checks if the player is allowed to attack currently only being used from the movement script
+	public bool canAttack = true;
 
-    public Vector2 SpawnPos
-    {
-        get
-        {
-            return spawnPos;
-        }
-
-        set
-        {
-            spawnPos = value;
-        }
-    }
-
-    void Start()
-    {
-        // Set the private variable to the public one
-        attackCooldown = attackSpeed;
-        hitbox.GetComponent<Hitbox>().isEnemy = false;
-        hitbox.GetComponent<Hitbox>().damage = attackDamage;
+	void Start()
+	{
+		// Set the private variable to the public one
+		attackCooldown = attackSpeed;
+		hitbox.GetComponent<Hitbox>().isEnemy = false;
+		hitbox.GetComponent<Hitbox>().damage = attackDamage;
 		cameraComponent = camera.GetComponent<Camera>();
 		animator = GetComponent<Animator>();
 		playerMovement = GetComponent<PlayerMovement>();
-    }
-
-    void Attack()
-    {
-        if (canAttack) {
-            Vector3 position = cameraComponent.ScreenToWorldPoint(Input.mousePosition);
-            position.z = 0;
-
-            Vector3 direction = Vector3.Normalize(position - transform.position);
-
-            Debug.Log("Dir: " + direction);
-
-            // Spawn hitbox in front of the player in the direction they are facing
-            Hitbox newHitbox = Instantiate(hitbox, transform.position + direction, transform.rotation).GetComponent<Hitbox>();
-
-            newHitbox.damage = attackDamage;
-            newHitbox.isEnemy = false;
-
-            playerMovement.timeTillCanMove = movePauseTime;
-
-            timeTillResetAnim = attackAnimTime;
-
-            animator.SetBool("Melee", true);
-        }
 	}
 
-    void Update()
-    {
+	void Attack()
+	{
+		if (canAttack)
+		{
+			Vector3 position = cameraComponent.ScreenToWorldPoint(Input.mousePosition);
+			position.z = 0;
+
+			Vector3 direction = Vector3.Normalize(position - transform.position);
+
+			Debug.Log("Dir: " + direction);
+
+			// Spawn hitbox in front of the player in the direction they are facing
+			Hitbox newHitbox = Instantiate(hitbox, transform.position + direction, transform.rotation).GetComponent<Hitbox>();
+
+			newHitbox.damage = attackDamage;
+			newHitbox.isEnemy = false;
+
+			playerMovement.timeTillCanMove = movePauseTime;
+
+			timeTillResetAnim = attackAnimTime;
+
+			animator.SetBool("Melee", true);
+		}
+	}
+
+	void Update()
+	{
 		timeTillResetAnim -= Time.deltaTime;
 
 		if (timeTillResetAnim <= 0.0f)
 			animator.SetBool("Melee", false);
 
-        // check if the attack cooldown has run out
-        if (attackCooldown <= 0)
-        {
-            // If we recieve players input
-            if (Input.GetMouseButtonDown(0))
-            {
-                // Run the script with attack code
-                Attack();
+		// check if the attack cooldown has run out
+		if (attackCooldown <= 0)
+		{
+			// If we recieve players input
+			if (Input.GetMouseButtonDown(0))
+			{
+				// Run the script with attack code
+				Attack();
 
-                // Reset the attackCooldown
-                attackCooldown = attackSpeed;
-            }
-        }
-        else // Reduce the attackCooldown
-            attackCooldown -= Time.deltaTime;
+				// Reset the attackCooldown
+				attackCooldown = attackSpeed;
+			}
+		}
+		else // Reduce the attackCooldown
+			attackCooldown -= Time.deltaTime;
 
-    }
+	}
 }
