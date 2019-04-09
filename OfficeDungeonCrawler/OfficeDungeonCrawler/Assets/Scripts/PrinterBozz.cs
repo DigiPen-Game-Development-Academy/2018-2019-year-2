1548ff1e84983604b1cb6968e9c4bfbb8a0bc19a
justanmeemman <45608802+justanmeemman@users.noreply.github.com>
2019-04-09 15:07:13 -0700

    call in all with the pick and roll yung la flame he in sicko mode

---
+++ OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/PrinterBozz.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrinterBozz : MonoBehaviour {
+    public bool moving = false;
+    public bool shooting = false;
+    public float switchSpeed = 1;
+    public float CurrentHealth = 0;
+
+    [HideInInspector]
+    public float timer = 0;
+    [HideInInspector]
+    public float Health;
+    
+	// Use this for initializon
+	void Start () {
+       Health = gameObject.GetComponent<Health>().maxHealth;
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CurrentHealth = gameObject.GetComponent<Health>().currentHealth;
+
+        if (CurrentHealth >= 0.90 * Health && CurrentHealth <= 0.75 * Health)
+        {
+
+
+
+
+        }
+        if(CurrentHealth <= 0.25 * Health && CurrentHealth >= 0.50 * Health)
+        {
+
+        }
+        if (CurrentHealth <= 0.25 * Health && CurrentHealth >= 0.50 * Health)
+        {
+
+        }
+        if (CurrentHealth <= 0.25 * Health && CurrentHealth >= 0.50 * Health)
+        {
+
+        }
+            timer += Time.deltaTime * switchSpeed;
+	}
+}
