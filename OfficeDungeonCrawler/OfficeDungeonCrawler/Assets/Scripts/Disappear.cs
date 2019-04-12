4f3fdfb5f4e1be6ecd87a290b4d65ee666bb0a4b
deleted-user <>
2019-04-12 13:36:05 -0700

    e

---
+++ OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/Disappear.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disappear : MonoBehaviour
+{
+	public float speed = 2.0f;
+	public float range = 0.1f;
+
+	SpriteRenderer spriteRenderer;
+
+	void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	void Update()
+	{
+		spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, Mathf.Lerp(spriteRenderer.color.a, 0.0f, Time.deltaTime * speed));
+
+		if (spriteRenderer.color.a <= range)
+			Destroy(gameObject);
+	}
+}
