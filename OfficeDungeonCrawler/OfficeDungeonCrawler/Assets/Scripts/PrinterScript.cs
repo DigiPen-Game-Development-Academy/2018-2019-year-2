7fb59fa183ea1e0cb0b09b1686f29c691d356aee
deleted-user <>
2019-04-13 16:22:30 -0700

    e

--- OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/PrinterScript.cs
+++ OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/PrinterScript.cs
@@ -10,132 +10,143 @@ using UnityEngine;
 
 public class PrinterScript : MonoBehaviour
 {
-    public GameObject projectile;
-    GameObject player;
-    public bool bossConditions = true;
-    public float projectileSpeed;
-    public float projectileLifespan;
-    public float detectionRange;
-    public float firerate = 0;
-    public int burstSize = 3;
-    public float attackDamage;
-    public float turnSpeedInDegrees;
-    public int shotsLeft = 0;
-    public Sprite sprite1;
-    public Sprite sprite2;
-    public Sprite sprite3;
-    bool lineOfSight = false;
-    public float fireRate = 5;
-    public LayerMask m_layerMask;
-    public float burstDuration = 0;
-    [HideInInspector]
-    public bool allowedFire = false;
-    [HideInInspector]
-    public float timer = 0;
-    [HideInInspector]
-    public float burstTimer = 0;
-    public float burstTimeCoolDown = 0;
-    public Sprite ShootingPrinterFront;
-    public Sprite notShootingPrinter;
-     void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-        timer = firerate;
-        shotsLeft = burstSize;
-        fireRate = Time.deltaTime * fireRate;
-        if (burstTimeCoolDown > 0)
-            burstTimeCoolDown = -burstTimeCoolDown;
-    }
+	public GameObject projectile;
+	GameObject player;
+	public bool bossConditions = true;
+	public float projectileSpeed;
+	public float projectileLifespan;
+	public float detectionRange;
+	public float firerate = 0;
+	public int burstSize = 3;
+	public float attackDamage;
+	public float turnSpeedInDegrees;
+	public int shotsLeft = 0;
+	public Sprite sprite1;
+	public Sprite sprite2;
+	public Sprite sprite3;
+	bool lineOfSight = false;
+	public float fireRate = 5;
+	public LayerMask m_layerMask;
+	public float burstDuration = 0;
+	[HideInInspector]
+	public bool allowedFire = false;
+	[HideInInspector]
+	public float timer = 0;
+	[HideInInspector]
+	public float burstTimer = 0;
+	public float burstTimeCoolDown = 0;
+	public Sprite ShootingPrinterFront;
+	public Sprite notShootingPrinter;
+	public float signalTime = 0.5f;
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		timer = firerate;
+		shotsLeft = burstSize;
+		fireRate = Time.deltaTime * fireRate;
+		if (burstTimeCoolDown > 0)
+			burstTimeCoolDown = -burstTimeCoolDown;
+	}
 
-    void Update()
-    {
-        
-        
-        if (Vector2.Distance(transform.position, player.transform.position) <= detectionRange)
-        {
-            //OLD CODE
-            //Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, Mathf.Infinity);
-            //Debug.DrawRay(transform.position, dir * 10, Color.red, 100f, false);
+	void Update()
+	{
 
-            //-----------------//
-            //CHRIS ONORATI CODE
-            Vector3 dir = player.transform.position - transform.position + (Vector3)player.GetComponent<CircleCollider2D>().offset;
-            dir.Normalize();
 
-            //just a different method to make a raycast - I recommend avoiding out.
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 100.0f, m_layerMask);
+		if (Vector2.Distance(transform.position, player.transform.position) <= detectionRange)
+		{
+			//OLD CODE
+			//Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, Mathf.Infinity);
+			//Debug.DrawRay(transform.position, dir * 10, Color.red, 100f, false);
 
-            //END CHRIS CODE
-            //-----------------//
-                        
-                burstTimer += Time.deltaTime;
+			//-----------------//
+			//CHRIS ONORATI CODE
+			Vector3 dir = player.transform.position - transform.position + (Vector3)player.GetComponent<CircleCollider2D>().offset;
+			dir.Normalize();
 
-            //Debug.Log(hit.collider.tag);
-            if (hit.collider.tag.Equals("Player"))
-            {
-                //Debug.Log("In LINE OF SIGHT");
-                lineOfSight = true;
-            }
-            else
-            {
-                //Debug.Log("OUT OF SIGHT");
-                lineOfSight = false;
-            }
-                if (lineOfSight)
-                {
-                    Vector3 difference = player.transform.position - transform.position;
-                    Vector3 newRotation = Vector3.RotateTowards(transform.right, difference, Mathf.Deg2Rad * turnSpeedInDegrees * Time.deltaTime, Vector3.Distance(transform.position, player.transform.position));
-                    transform.right = newRotation;
+			//just a different method to make a raycast - I recommend avoiding out.
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 100.0f, m_layerMask);
 
-                    if (bossConditions)
-                    {
-                        if (timer <= 0 && allowedFire == true)
-                        {
-                            Debug.Log("Shoot Player");
-                            Sprite spriteToSpawn = sprite1;
+			//END CHRIS CODE
+			//-----------------//
 
-                            int randomNumber = Random.Range(0, 6);
-                            if (randomNumber < 2)
-                                spriteToSpawn = sprite1;
-                            if (randomNumber > 2 && randomNumber < 4)
-                                spriteToSpawn = sprite2;
-                            if (randomNumber > 4)
-                                spriteToSpawn = sprite3;
+			burstTimer += Time.deltaTime;
 
-                            Quaternion rotation = transform.rotation;
-                            GameObject newProjectile = Instantiate(projectile, transform.position + transform.right, rotation *= Quaternion.Euler(0, 0, 90));
+			//Debug.Log(hit.collider.tag);
+			if (hit.collider.tag.Equals("Player"))
+			{
+				//Debug.Log("In LINE OF SIGHT");
+				lineOfSight = true;
+			}
+			else
+			{
+				//Debug.Log("OUT OF SIGHT");
+				lineOfSight = false;
+			}
+			if (lineOfSight)
+			{
+				Vector3 difference = player.transform.position - transform.position;
+				Vector3 newRotation = Vector3.RotateTowards(transform.right, difference, Mathf.Deg2Rad * turnSpeedInDegrees * Time.deltaTime, Vector3.Distance(transform.position, player.transform.position));
+				transform.right = newRotation;
 
-                            newProjectile.transform.localScale = new Vector3(2, 2, 2);
-                            newProjectile.GetComponent<Hitbox>().isEnemy = true;
-                            newProjectile.GetComponent<Hitbox>().damage = attackDamage;
-                            newProjectile.GetComponent<TimedDeath>().deathTimer = projectileLifespan;
+				if (bossConditions)
+				{
+					if (timer <= 0 && allowedFire == true)
+					{
+						Debug.Log("Shoot Player");
+						Sprite spriteToSpawn = sprite1;
 
-                            newProjectile.GetComponent<SpriteRenderer>().sprite = spriteToSpawn;
-                            newProjectile.GetComponent<Rigidbody2D>().velocity = transform.right * projectileSpeed;
+						int randomNumber = Random.Range(0, 6);
+						if (randomNumber < 2)
+							spriteToSpawn = sprite1;
+						if (randomNumber > 2 && randomNumber < 4)
+							spriteToSpawn = sprite2;
+						if (randomNumber > 4)
+							spriteToSpawn = sprite3;
 
-                            timer = firerate;
-                        }
-                        else
-                        {
-                            timer -= fireRate;
-                        }
-                }
+						Quaternion rotation = transform.rotation;
+						GameObject newProjectile = Instantiate(projectile, transform.position + transform.right, rotation *= Quaternion.Euler(0, 0, 90));
 
-                    if (burstTimer >= 0)
-                    {
-                        Debug.Log("Burst timer ended");
-                        allowedFire = true;
-                        GetComponent<SpriteRenderer>().sprite = ShootingPrinterFront;
-                    }
-                    if (burstTimer >= burstDuration && allowedFire == true)
-                    {
-                        GetComponent<SpriteRenderer>().sprite = notShootingPrinter;
-                        allowedFire = false;
-                        burstTimer = burstTimeCoolDown;
-                    }
+						newProjectile.transform.localScale = new Vector3(2, 2, 2);
+						newProjectile.GetComponent<Hitbox>().isEnemy = true;
+						newProjectile.GetComponent<Hitbox>().damage = attackDamage;
+						newProjectile.GetComponent<TimedDeath>().deathTimer = projectileLifespan;
 
-                }
-            }
-        }
-    }
+						newProjectile.GetComponent<SpriteRenderer>().sprite = spriteToSpawn;
+						newProjectile.GetComponent<Rigidbody2D>().velocity = transform.right * projectileSpeed;
+
+						timer = firerate;
+					}
+					else
+					{
+						timer -= fireRate;
+					}
+				}
+
+				if (burstTimer >= 0)
+				{
+					Debug.Log("Burst timer ended");
+					allowedFire = true;
+					
+				}
+				if (burstTimer >= -signalTime)
+				{
+					GetComponent<SpriteRenderer>().sprite = ShootingPrinterFront;
+
+					SpriteRenderer csr = transform.Find("Glow").GetComponent<SpriteRenderer>();
+					csr.color = new Color(1.0f, 0.0f, 0.0f, csr.color.a);
+				}
+				if (burstTimer >= burstDuration && allowedFire == true)
+				{
+					GetComponent<SpriteRenderer>().sprite = notShootingPrinter;
+					allowedFire = false;
+					burstTimer = burstTimeCoolDown;
+
+					SpriteRenderer csr = transform.Find("Glow").GetComponent<SpriteRenderer>();
+					csr.color = new Color(0.0f, 1.0f, 0.0f, csr.color.a);
+				}
+
+			}
+		}
+	}
+}
 
