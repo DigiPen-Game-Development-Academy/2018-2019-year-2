247fedafd9a0805b3a294d351ddc212684682392
deleted-user <>
2019-04-13 14:33:32 -0700

    e

---
+++ OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/Key.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+	public GameObject door;
+	public AudioClip unlockSound;
+
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			door.GetComponent<DoorScript>().locked = false;
+			GameObject.Find("KeyAudio").GetComponent<AudioSource>().PlayOneShot(unlockSound, 2.0f);
+			Destroy(gameObject);
+		}
+	}
+}
