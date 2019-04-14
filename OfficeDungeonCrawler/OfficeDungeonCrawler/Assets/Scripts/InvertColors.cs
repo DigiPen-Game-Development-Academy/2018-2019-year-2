2a014b9af947ef1fa1d08725afb129df5fd8f05d
deleted-user <>
2019-04-14 12:00:58 -0700

    e

---
+++ OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/InvertColors.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvertColors : MonoBehaviour
+{
+	public bool active = false;
+	public Material mat;
+
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		
+	}
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		if (!active)
+		{
+			Graphics.Blit(source, destination);
+			return;
+		}
+
+		Graphics.Blit(source, destination, mat);
+	}
+}
