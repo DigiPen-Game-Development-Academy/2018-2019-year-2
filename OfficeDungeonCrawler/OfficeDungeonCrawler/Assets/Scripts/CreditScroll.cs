658aac64b37529c2f2805a2e431cc65db648e45f
deleted-user <>
2019-04-19 13:26:51 -0700

    e

---
+++ OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/CreditScroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditScroll : MonoBehaviour
+{
+	public float max = -100.0f;
+	public float speed = 0.02f;
+
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		if (transform.position.y + speed <= max)
+			transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+	}
+}
