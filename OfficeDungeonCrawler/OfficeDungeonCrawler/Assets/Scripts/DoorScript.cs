575578fd35ab5bb8a3d5362068203fe753c32105
deleted-user <>
2019-04-23 11:12:17 -0700

    e

--- OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/DoorScript.cs
+++ OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/DoorScript.cs
@@ -9,13 +9,33 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
+public enum DoorUnlockMode
+{
+	None,
+	ObjectDestroyed,
+	LeftClick,
+	RightClick,
+	Dash
+}
+
 public class DoorScript : MonoBehaviour
 {
 	public string nextRoom;
 	public bool locked = true;
+	public DoorUnlockMode unlockMode = DoorUnlockMode.None;
+	public GameObject obj = null;
 
 	void Update()
 	{
+		if (unlockMode == DoorUnlockMode.ObjectDestroyed && obj == null)
+			locked = false;
+		if (unlockMode == DoorUnlockMode.LeftClick && Input.GetMouseButtonDown(0))
+			locked = false;
+		if (unlockMode == DoorUnlockMode.RightClick && Input.GetMouseButtonDown(1))
+			locked = false;
+		if (unlockMode == DoorUnlockMode.Dash && Input.GetKeyDown(KeyCode.LeftShift))
+			locked = false;
+
 		if (locked)
 			transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
 		else
---
+++ OfficeDungeonCrawler/OfficeDungeonCrawler/Assets/Scripts/Splash.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+	public float speed = 0.02f;
+	public float stayTime = 2.0f;
+	public float delay = 0.0f;
+	float time = 0.0f;
+	public string level;
+	public float switchDelay = 1.0f;
+
+	bool faded = false;
+
+	SpriteRenderer sr;
+	TextMesh t;
+
+	void Start()
+	{
+		sr = GetComponent<SpriteRenderer>();
+		t = GetComponent<TextMesh>();
+	}
+	
+	void Update()
+	{
+		if (time < delay)
+		{
+			time += Time.deltaTime;
+			return;
+		}
+
+		if (sr != null)
+		{
+			if (sr.color.a >= 1.0f)
+				faded = true;
+		}
+		if (t != null)
+		{
+			if (t.color.a >= 1.0f)
+				faded = true;
+		}
+
+		if (!faded)
+		{
+			if (sr != null)
+				sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, sr.color.a + speed);
+			if (t != null)
+				t.color = new Color(t.color.r, t.color.g, t.color.b, t.color.a + speed);
+		}
+		else
+			time += Time.deltaTime;
+
+		if (time >= delay + stayTime)
+		{
+			if (sr != null)
+				sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, sr.color.a - speed);
+			if (t != null)
+				t.color = new Color(t.color.r, t.color.g, t.color.b, t.color.a - speed);
+		}
+
+		if (sr != null)
+		{
+			if (sr.color.a <= 0.0f && faded && time >= delay + stayTime + switchDelay && level != "")
+				SceneManager.LoadScene(level);
+		}
+		if (t != null)
+		{
+			if (t.color.a <= 0.0f && faded && time >= delay + stayTime + switchDelay && level != "")
+				SceneManager.LoadScene(level);
+		}
+	}
+}
